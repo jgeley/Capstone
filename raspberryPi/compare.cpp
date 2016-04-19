546e064c9640bc3e5ef4cc1b78cb271cadaa6fc9
jgeley <jeley@unomaha.edu>
2016-04-19 16:27:57 -0500

    adding history csv files and age and gender estimation

--- raspberryPi/compare.cpp
+++ raspberryPi/compare.cpp
@@ -7,6 +7,7 @@
 #include <ctime>
 #include <sstream>
 #include <string>
+#include <fstream>
 
 
 int compare(int argc, char *argv[], char const *tempfile)
@@ -49,17 +50,58 @@ int compare(int argc, char *argv[], char const *tempfile)
 	query >> *transform;
 	QList<float> scores = distance->compare(target,query);
 	int highest = -1;
+	QString highestName = "";
 	for (int i = 0; i < scores.count(); ++i){
 
 		printf("%s and %s score:  %.3f\n",qPrintable(target[i].file.name),qPrintable(query.file.name),scores[i]);
 		if(scores[i] > highest){
+			highestName = target[i].file.name;
 			highest = scores[i];
 		}
 	}
 	if(highest > 3){
-		std::cout << "You have been here before";
+		std::cout << "You have been here before\n";
+		std::ofstream out;
+                out.open("historytemp.csv",std::ios::trunc);
+                //out <<  tempfile << "," << "0\n";
+                //out.close();
+		std::ifstream data("history.csv");
+                std::string line;
+                //std::cout << "-------------------------------------------\n";
+                while(std::getline(data,line)){
+		      std::vector<std::string> row;
+                      std::stringstream lineStream(line);
+                      std::string cell;
+                      while(std::getline(lineStream,cell,',')){
+			      row.push_back(cell);
+                              //std::cout << cell << "\n";
+                      }
+		      QString current = QString(row[0].c_str());
+			if(highestName.compare(current) == 0){
+				std::cout << qPrintable(current);
+				int value = atoi(row[1].c_str());
+				value = value + 1;
+				std::ostringstream oss;
+				oss << row[0] << "," << value << "," << row[2] << "," <<row[3];			
+				out << oss.str() << "\n";
+			}else {
+				out << line  << "\n";
+			}
+                }
+                out.close();
+		data.close();
+		// copy over historytemp to history file
+		out.open("history.csv",std::ios::trunc);
+		std::ifstream data2("historytemp.csv");
+		while(std::getline(data2,line)){
+			out << line << "\n";
+		}
+		out.close();
+		data2.close();
+                //std::cout << "---------------------------------------------\n";
 	}
 	else {
+		// Makes gallery
 		std::cout << "You are new here, your picture will be added to our database";
 		time_t t = time(0);
 		struct tm * now = localtime(&t);
@@ -77,8 +119,23 @@ int compare(int argc, char *argv[], char const *tempfile)
 		br::File gallery(qstr);
 		br::File file(tempfile);
 		br::Globals->setProperty("algorithm","FaceRecognition");
-		br::Enroll(file,gallery); 			
+		br::Enroll(file,gallery);
+		// add entry to history.csv
+		std::ofstream out;
 		
+		// get age estimation
+		QSharedPointer<br::Transform> ageTransform = br::Transform::fromAlgorithm("AgeEstimation");
+		br::Template ageQuery(tempfile);
+		ageQuery >> *ageTransform;
+		int age = int(ageQuery.file.get<float>("Age"));
+		// get gender
+		QSharedPointer<br::Transform> genderTransform = br::Transform::fromAlgorithm("GenderEstimation");
+		br::Template genderQuery(tempfile);
+		genderQuery >> *genderTransform;
+		QString gender = genderQuery.file.get<QString>("Gender");
+		out.open("history.csv",std::ios::app);
+		out <<  tempfile << "," << "0," << age << "," << qPrintable(gender) << "\n";
+		out.close();			
 	}
 	br::Context::finalize();
 	return 0;
--- raspberryPi/runner.cpp
+++ raspberryPi/runner.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <cv.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <ctime>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <openbr/openbr_plugin.h>
 #include <iostream>
@@ -23,10 +24,24 @@ int main(int argc, char *argv[])
 		//		std::cout << "Program exited abnormally";
 		//	}
 		//}
+		time_t t = time(0);
+                struct tm * now = localtime(&t);
+                int year = now->tm_year + 1900;
+                int mon = now->tm_mon + 1;
+                int day = now->tm_mday +1;
+                int hour= now->tm_hour;
+                int min = now->tm_min;
+                int sec = now->tm_sec;
+                std::string str = "";
+                std::ostringstream oss;
+                oss << "images/" << mon << "" << day << "" << year << "" << hour << "" << min << "" << sec << ".jpg";
+                std::string mys = oss.str();
 		std::cout << "after pic\n";
-		system("raspistill -o temp.jpg -t 100");
-		std::string tempstr = "temp.jpg";
-                compare(argc,argv,tempstr.c_str());
+		std::ostringstream oss2;
+		oss2 << "raspistill -o " << mys << " -t 100";
+		system(oss2.str().c_str());
+		//std::string tempstr = "temp.jpg";
+                compare(argc,argv,mys.c_str());
 		std::cout << "after compare\n";
 		
 	}
