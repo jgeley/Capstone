94a9b52e0fcd5a5195f5dcb0958cefa8249df525
jgeley <jeley@unomaha.edu>
2016-04-26 21:17:10 -0500

    adding age, gender, and times

--- cpp/app.py
+++ cpp/app.py
@@ -26,18 +26,19 @@ while run :
         proc.stdin.flush()
         screen.fill((0,0,0))
         fl = proc.stdout.readline().strip().split()
-	if fl[0] == "FACE" :
-            np = int(fl[1])
-            nr = int(fl[2])
-            print(proc.stdout.readline())
+	print fl
+	#if fl[0] == "FACE" :
+            #np = int(fl[1])
+            #nr = int(fl[2])
+            #print(proc.stdout.readline())
             #print(list(map(float,proc.stdout.readline().strip().split())))
-            for i in range(np) :
-                point = tuple(map(lambda x : int(float(x)),proc.stdout.readline().strip().split()))
-                pygame.draw.circle(screen,(255,255,255),point,5,2)
-            for i in range(nr) :
-                rect = tuple(map(lambda x : int(float(x)),proc.stdout.readline().strip().split()))
-                pygame.draw.rect(screen,(100,100,100),rect,5)
-        pygame.display.flip()
+            #for i in range(np) :
+            #    point = tuple(map(lambda x : int(float(x)),proc.stdout.readline().strip().split()))
+            #    pygame.draw.circle(screen,(255,255,255),point,5,2)
+            #for i in range(nr) :
+            #    rect = tuple(map(lambda x : int(float(x)),proc.stdout.readline().strip().split()))
+            #    pygame.draw.rect(screen,(100,100,100),rect,5)
+        #pygame.display.flip()
         print("FRAME")
     except Exception :
         traceback.print_exc()
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,6 +6,7 @@
 #include <highgui.h>
 #include <unistd.h>
 
+
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -14,6 +15,7 @@ int main(int argc, char* argv[])
 	QSharedPointer<br::Transform> transform = br::Transform::fromAlgorithm("FaceRecognition");
 	QSharedPointer<br::Transform> ageTransform = br::Transform::fromAlgorithm("AgeEstimation");
 	QSharedPointer<br::Distance> distance = br::Distance::fromAlgorithm("FaceRecognition");
+	QSharedPointer<br::Transform> genderTransform = br::Transform::fromAlgorithm("GenderEstimation");
 
 	cout<< endl << "STARTING"<<endl ;
 	cout<< flush ;
@@ -27,13 +29,10 @@ int main(int argc, char* argv[])
 			cin >> source;
 			br::Template thing(source);
 			br::Template age = thing.clone();
+			br::Template gender = thing.clone();
 			thing >> *transform;
-			age >> *ageTransform;
 
 			//cv::imwrite("output.jpg",thing.m());
-			//cout<<thing.m()<<endl;
-			//cout << thing.file.m_metadata <<endl;
-			//cout << thing.file.flat().toStdString() <<endl;
 			if( thing.file.contains("Confidence") ) {
 				float confidence = thing.file.get<float>("Confidence");
 				if( confidence > 0.0 ){
@@ -45,29 +44,27 @@ int main(int argc, char* argv[])
 					struct dirent *dir;
 					d = opendir("templates");
 					if (d){
-                        int count = 0;
+						int count = 0;
 						while((dir = readdir(d)) != NULL){
-                            char* thing = (char*) malloc(strlen(dir->d_name)+1);
-                            strcpy(thing, dir->d_name);
+							char* thing = (char*) malloc(strlen(dir->d_name)+1);
+							strcpy(thing, dir->d_name);
 							templateList.push_back(string(thing));
-/*                            cerr << (void*)(dir->d_name) << endl;
-                            const char* thing = tmpstr.c_str();
-                            cerr << (void*)tmpstr.data() << endl;*/
+							/*                            cerr << (void*)(dir->d_name) << endl;
+										      const char* thing = tmpstr.c_str();
+										      cerr << (void*)tmpstr.data() << endl;*/
 						}
 						closedir(d);
 					}
-					
 					// do compare
 					bool foundmatch = false;
 					br::Template result;
 					for(unsigned int j = 0; j<templateList.size(); j--){
 						//char *s = new char[templateList.at(j).length() + 1];
-                        templateList[j].c_str();
-                        string x = templateList.at(j);//strcpy(s,templateList.at(j).c_str());
+						templateList[j].c_str();
+						string x = templateList.at(j);//strcpy(s,templateList.at(j).c_str());
 						const char *s = x.c_str();
 						if(strstr(s,".tmpl") != NULL){
 							// this is getting the file from the template directory
-							//cout  << templateList[j].c_str() << "\n\n\n\n\n";
 							char base[512] = "templates/";
 							strcat(base,s);
 							const char * destPtr = (const char *)base;
@@ -77,43 +74,54 @@ int main(int argc, char* argv[])
 							br::Template templ;	
 							in >> templ;
 							float score = distance->compare(templ, thing);
+							//cout << score << " ";
 							if(score > 3){
 								result = templ.clone();
 								foundmatch = true;
-								//cout << "FOUND MATCH------------------------------------------------\n";
 							}
-							//cout << flush;
 						}
 					}		
-					
+					//cout << "\n";
+					//cout << flush;
 					// If a match is not found
-					if(true){
+					if(!foundmatch){
+						gender >> *genderTransform;
 						age >> *ageTransform;
 						QString base = "templates/";
 						base.append(thing.file.hash());
 						base.append(".tmpl");
-						cout << "going to open file " << qPrintable(thing.file.hash()) << " \n";
 						QFile out(base);
 						out.open(QIODevice::WriteOnly);
 						QDataStream outstream(&out);
+						thing.file.set("Age",QVariant::fromValue<float>(age.file.get<float>("Age")));
+						thing.file.set("Gender",QVariant::fromValue<QString>(gender.file.get<QString>("Gender")));
+						thing.file.set("Times",QVariant::fromValue<int>(1));
 						outstream << thing;
 						QList<QPointF> points = thing.file.points();
 						QList<QRectF> rects = thing.file.rects();
-						cout << "FACE "<<points.length()<<" "<<rects.length()<<endl;
-						cout << age.file.flat().toStdString() <<endl;
+						cout << "FACE " << "1" << " " << qPrintable(gender.file.get<QString>("Gender")) << " " << age.file.get<float>("Age") <<endl;
+						//cout << thing.file.flat().toStdString() <<endl;
+						//cout << flush;
 						/*cv::Mat matrix = thing.m();
 						  for(cv::MatConstIterator_<double> it = matrix.begin<double>();it != matrix.end<double>();it++) {
-						  cout << *it << " ";
+						  //cout << *it << " ";
 						  }
-						  cout << endl;*/
+						  //cout << endl;*/
 						for(int i=0;i<points.length();i++) {
-							cout << points[i].x() << " " << points[i].y() << endl;
+							//cout << points[i].x() << " " << points[i].y() << endl;
 						}
 						for(int i=0;i<rects.length();i++) {
-							cout << rects[i].x() << " " << rects[i].y() << " ";
-							cout << rects[i].width() << " " << rects[i].height() << endl;
+							//cout << rects[i].x() << " " << rects[i].y() << " ";
+							//cout << rects[i].width() << " " << rects[i].height() << endl;
 						}
 					} else {
+						int numTimes = result.file.get<int>("Times");
+						numTimes += 1;
+						//cout << "num times " << numTimes << "\n";
+						//cout << result.file.flat().toStdString() << endl;
+						//cout << flush;
+						result.file.set("Times",QVariant::fromValue<int>(numTimes));
+						cout << "FACE "  << result.file.get<int>("Times") << " " << qPrintable(result.file.get<QString>("Gender")) << " " << result.file.get<float>("Age") << endl;
 						// if a match is not found (increment)	
 					}
 				} else {
